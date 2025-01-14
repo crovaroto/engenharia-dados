fb1adc51547802fe021fade40932d538b8617b28
crovaroto <cassio.rovaroto@gmail.com>
2025-01-13 21:14:48 -0300

    Incluindo os fontes de exemplos mais o desafio.

---
+++ Python/Funcoes.py
@@ -0,0 +1,16 @@
+def exibir_mensagem():
+  print("Olá mundo.")
+
+def exibir_mensagem_2(nome):
+  print(f"Olá {nome}")
+
+def exibir_mensagem_3(nome="Anônimo"):
+  print(f"Olá {nome}")
+
+exibir_mensagem()
+exibir_mensagem_2("Cássio")
+exibir_mensagem_3()
+exibir_mensagem_3("Doidão")
+
+
+#Args e Kwargs
